1d4ec20a3e9c332f84ff8999218cc43ca2337f40
Kizito Azubuike <kizito.azubuike1@gmail.com>
2023-01-20 10:56:31 +0100

    compile sass to css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/styles.scss'
+import './styles/css/styles.css'
 import App from './App'
 import { MusicContextProvider } from './contexts/MusicContext'
 
